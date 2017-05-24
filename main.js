4f9e5af34dcb28162399b1909a35add945ff412f
SimonIdf <sametoall@163.com>
2017-05-24 13:43:06 +0800

    123
    123

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
 var WaitingHall = require("./WaitingHall.js");
 var MatchCenter = require("./MatchCenter.js");
 
-
-// 加载http模块和url模块
-var http = require("http");
-var url = require('url');
-
-var server = http.createServer(function(request, response) {
-    // url请求参数{"name":"aty","age":26}
-    var args = url.parse(request.url, true).query;
-
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<html>");
-    response.write("<head>");
-    response.write("<title>Hello World Page</title>");
-    response.write("</head>");
-    response.write("<body>");
-    response.write("Hello World!");
-    response.write("</body>");
-    response.write("</html>");
-    response.end();
-});
-
-server.listen(8999);
-
-console.log("server is listening");
+console.log("server running");
